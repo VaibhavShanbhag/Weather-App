b49fd2d0787215fc3fd4c2f519b4cf6c1751d4d4
Vaibhav Shanbhag <vaibhavshanbhag467@gmail.com>
2022-03-08 00:30:29 +0530

    Addded the volley library for network call

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,173 @@
 package com.example.weatherapp
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import java.util.jar.Manifest
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    lateinit var weatherModelArrayList: ArrayList<WeatherModel>
+    lateinit var adapter: Adapter
+    lateinit var weatherRV: RecyclerView
+    lateinit var locationManager: LocationManager
+    var PERMISSION_CODE: Int = 1
+    lateinit var CityName: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         setContentView(R.layout.activity_main)
+
+        weatherRV = findViewById(R.id.RVTodayForecast)
+
+        weatherModelArrayList = ArrayList()
+        adapter = Adapter(this, weatherModelArrayList)
+        weatherRV.adapter = adapter
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                PERMISSION_CODE
+            )
+        }
+
+        var location: Location? =
+            locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+        if (location != null) {
+            CityName = getCityName(location.longitude, location.latitude)
+        }
+
+        getWeatherInfo(CityName)
+
+        IVSearch.setOnClickListener {
+            var city: String = TECityLocation.text.toString()
+
+            if (city.isEmpty()) {
+                Toast.makeText(this, "Please Enter City Name", Toast.LENGTH_SHORT).show()
+            } else {
+                cityname.text = CityName
+                getWeatherInfo(city)
+            }
+        }
+
+
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+
+    }
+
+        fun getCityName(longitude: Double, latitude: Double): String {
+            var cityName: String = "Not Found"
+            var gcd: Geocoder = Geocoder(baseContext, Locale.getDefault())
+
+            try {
+                var address: List<Address> = gcd.getFromLocation(latitude, longitude, 10)
+
+                for (adr: Address in address) {
+                    if (adr != null) {
+                        val city = adr.locality
+                        if (city != null && !city.equals("")) {
+                            cityName = city
+                        } else {
+                            Log.d("Location", "City Not Found")
+                            Toast.makeText(this, "City Not Found", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            return cityName
+        }
+
+        fun getWeatherInfo(cityName: String) {
+            var url: String =
+                "http://api.weatherapi.com/v1/forecast.json?key=e66803847543443fa01142645220703&q=${cityName}&days=1&aqi=yes&alerts=yes"
+            cityname.text = cityName
+
+            val Queue = Volley.newRequestQueue(this)
+
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    loaddata.visibility = View.GONE
+                    homedata.visibility = View.VISIBLE
+                    weatherModelArrayList.clear()
+
+                    val temperature = response.getJSONObject("current").getString("temp_c")
+
+                    TVTemperature.text = temperature.plus("°C")
+
+                    val isDay = response.getJSONObject("current").getInt("is_day")
+                    val conditionText = response.getJSONObject("current").getJSONObject("condition").getString("text")
+                    val conditionIcon = response.getJSONObject("current").getJSONObject("condition").getString("icon")
+                    Picasso.get()
+                        .load("http://".plus(conditionIcon))
+                        .into(IVTemperature_Image)
+
+                    TVCondition.text = conditionText
+
+
+
+
+                },
+                Response.ErrorListener { error ->
+                    Toast.makeText(this,"Please Enter Valid City Name",Toast.LENGTH_SHORT).show()
+                }
+            );
+
+            Queue.add(jsonObjectRequest)
+        }
 }
